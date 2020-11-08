89541e921e4c489248c569d4dfb23fa58340d02a
GODTHOMASZ <66963274+GODTHOMASZ@users.noreply.github.com>
2020-11-08 17:50:28 +0500

    F

---
+++ lab5/Server/IService1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Server
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        DataTable GetData();
+
+        [OperationContract]
+        DataTable GetComboValue();
+
+        [OperationContract]
+        void NewRec(string cname, string service, string count, string date);
+    }
+}
